b851f0161aaf1a7305fbd4a957df88e2bb00836a
ParksFG <ParkerFGay@gmail.com>
2022-11-18 13:08:16 -0600

    Added functionality for getDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,8 +14,8 @@ const initdb = async () =>
 
 export const putDb = async (content) => {
   console.log('Posting to db');
-  const jateDB = await openDB('jate', 1);
-  const tx = jateDB.transaction('jate', 'readwrite');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({ value: content });
   const result = await request;
@@ -23,6 +23,14 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('Retrieving from db');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  return result;
+};
 
 initdb();
